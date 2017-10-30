d68bcd89debcd1c0e7073308041fc46c3d050a15
Luan Reis <luanReis@users.noreply.github.com>
2017-10-30 12:11:27 +0100

    Without tests

--- test/unit/filters/happinessMeter.spec.js
+++ test/unit/filters/happinessMeter.spec.js
@@ -2,27 +2,5 @@
 
 describe('happinessMeter', function() {
 	
-	var happinessMeterFilter;
-
-	beforeEach(module('conferenceApp'));
-
-	beforeEach(inject(function($injector) {
-		happinessMeterFilter = $injector.get('happinessMeterFilter');
-	}));
-
-	it('should return nice when given ruby', function() {
-		expect(happinessMeterFilter('ruby')).to.equal('Nice');
-	});
-
-	it('should return awesome when given python', function() {
-		expect(happinessMeterFilter('python')).to.equal('Awesome!');
-	});
-
-	it('should return ... when given javascript', function() {
-		expect(happinessMeterFilter('javascript')).to.equal('...');
-	});
-
-	it('should return great when given java', function() {
-		expect(happinessMeterFilter('java')).to.equal('Great');
-	});
+	
 });

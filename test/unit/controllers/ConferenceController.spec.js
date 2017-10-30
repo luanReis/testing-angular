99207a1d13fb6108a31457bc18d923ad20d075b2
Luan Reis <luanReis@users.noreply.github.com>
2017-10-30 12:12:23 +0100

    Without tests

--- test/unit/controllers/ConferenceController.spec.js
+++ test/unit/controllers/ConferenceController.spec.js
@@ -2,43 +2,5 @@
 
 describe('ConferenceController', function() {
 
-	var $controller, scope, mockConferenceData;
-
-	beforeEach(module('conferenceApp'));
-
-	beforeEach(inject(function($injector) {
-		$controller = $injector.get('$controller');
-		scope = $injector.get('$rootScope').$new();
-		mockConferenceData = sinon.stub({getAllConferences: function() {}});
-	}));
-
-	it('should fetch all available conferences', function() {
-		var mockConferences = [
-			{name: 'conference1'},
-			{name: 'conference2'}
-		];
-		mockConferenceData.getAllConferences.returns(mockConferences);
-
-		$controller('ConferenceController', {
-			$scope: scope,
-			conferenceData: mockConferenceData
-		});
-
-		scope.showConferences();
-
-		expect(scope.conferences).to.equal(mockConferences);
-	});
-
-	it('should return an empty list when there are no conferences', function() {
-		mockConferenceData.getAllConferences.returns([]);
-
-		$controller('ConferenceController', {
-			$scope: scope,
-			conferenceData: mockConferenceData
-		});
-
-		scope.showConferences();
-
-		expect(scope.conferences).to.deep.equal([]);
-	});
+	
 });
